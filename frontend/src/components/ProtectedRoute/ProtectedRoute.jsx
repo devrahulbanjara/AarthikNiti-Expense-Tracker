9f788badbf0319a598c8ad8d2026b7efbfef6e4b
devrahulbanjara <rdbanjara07@gmail.com>
2025-03-22 23:00:14 +0545

    Fix the protected route to login

--- frontend/src/components/ProtectedRoute/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -4,7 +4,7 @@ import { Navigate } from "react-router-dom";
 export const ProtectedRoute = ({ children }) => {
   const token = localStorage.getItem("token");
 
-  return token ? children : <Navigate to="/login" />;
+  return token ? children : <Navigate to="/" />;
 };
 
 export const PublicRoute = ({ children }) => {
