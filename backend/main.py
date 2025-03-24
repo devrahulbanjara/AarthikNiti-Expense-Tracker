f41b88be2f5794efb5a54bc97d4bec26f39702e4
devrahulbanjara <rdbanjara07@gmail.com>
2025-03-24 14:34:37 +0545

    Integrate the Google signin

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routes import auth, profile
+from starlette.middleware.sessions import SessionMiddleware
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
 
@@ -12,9 +16,14 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+SESSION_SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey123")
+app.add_middleware(SessionMiddleware, secret_key=SESSION_SECRET_KEY, session_cookie="session_id", same_site="lax")
+
+from routes import auth, profile
+
 app.include_router(auth.router)
 app.include_router(profile.router)
 
 @app.get("/")
 def home():
-    return {"message": "Welcome to AarthikNiti Expense Tracker API"}
+    return {"message": "Welcome to AarthikNiti Expense Tracker"}
