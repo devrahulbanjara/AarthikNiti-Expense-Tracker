074f47226108d7e5297e8b319d82b1a873a0454f
Rahul Dev Banjara <rdbanjara07@gmail.com>
2025-04-09 09:29:12 +0545

    Switch to new profile and change in backend

--- backend/core/security.py
+++ backend/core/security.py
@@ -2,7 +2,7 @@ from passlib.context import CryptContext
 import jwt
 import os
 from dotenv import load_dotenv
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from fastapi import HTTPException, status
 
 load_dotenv()
@@ -17,23 +17,17 @@ def hash_password(password: str) -> str:
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
-def create_jwt_token(data: dict, expires_delta: timedelta = timedelta(hours=200)) -> str:
-    """Creates a JWT token with an expiration time."""
+def create_jwt_token(data: dict, expires_delta: timedelta | None = None) -> str:
     to_encode = data.copy()
-    expire = datetime.utcnow() + expires_delta
-    to_encode.update({"exp": expire})
-    
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+        to_encode.update({"exp": expire})
     return jwt.encode(to_encode, JWT_SECRET, algorithm=JWT_ALGORITHM)
 
 def decode_jwt_token(token: str) -> dict:
-    """Decodes JWT and validates it."""
     try:
         payload = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
         return payload
-    except jwt.ExpiredSignatureError:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail="Token expired"
-        )
     except jwt.PyJWTError:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token"
